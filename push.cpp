522e2ac36a34477c0cf3e4c9d8f5e22c77228f6b
Bhanupratap4726 <146018401+Bhanupratap4726@users.noreply.github.com>
2023-09-27 09:02:23 +0530

    Create push.cpp

---
+++ push.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int top=-1;
+int size;
+int stack[100];
+void push(int x)
+{
+    if (top == size - 1)
+    {
+        printf("Stack is Full");
+    }
+    else
+    {
+        top++;
+        stack[top] = x;
+    }
+}
+
+int main(){
+    int x;
+    cin>>x;
+    cin>>size;
+    push(2);
+}
